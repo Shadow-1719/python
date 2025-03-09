0e941c9a9ba2429349e7ac0844bcd2511d31579b
Shadow-1719 <shadoweminence412@gmail.com>
2025-03-09 19:58:07 -0400

    daniel modo

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask
-import os
-
-app = Flask(__name__)  # Usa __name__ en lugar de "Aland"
+name = "main"
+app = Flask(name)
 
 @app.route('/')
 def hello_world():
-    return "Hola Aland!"
+    return "Hola Mundo"
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 3000))  # Usa la variable PORT de Render
-    app.run(host="0.0.0.0", port=port, debug=False)
+if name == 'main':
+    app.run(debug=True)
